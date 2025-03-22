5d235504b73c08104c51cce0202dbee411421469
850004123 <850004123@nzse.ac.nz>
2025-03-22 14:02:24 +1300

    update

--- GST calculator 2/Program.cs
+++ GST calculator 2/Program.cs
@@ -7,17 +7,56 @@ namespace GST_calculator_2
         static void Main(string[] args)
             // Written by Jordan.E
         {
+            // Ask user to enter product name
             Console.WriteLine("Please enter product name: ");
             string product = Console.ReadLine();
 
+            // Ask user to enter quantity
             Console.WriteLine("Please enter quantity: ");
             int quantity = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please enter price: ");
-            double price = Convert.ToDouble(Console.ReadLine());
+            // Ask user to enter product cost
+            Console.WriteLine("Please enter product cost: ");
+            double cost = Convert.ToDouble(Console.ReadLine());
 
+            // Ask user if GST is included in the price
             Console.WriteLine(" Is GST included in the price? (y/n)");
             string gstchoice = Console.ReadLine();
+
+            // Check user choice is GST included or not
+
+            if (gstchoice == "y")
+               
+
+            {
+              // Calculate amount excluding GST
+
+                double amountExcludingGST = cost/ 1.15;
+
+                // Display the total amount excluding GST
+
+                Console.WriteLine("The total amount excluding GST is: " + amountExcludingGST);
+
+            }
+            else if (gstchoice == "n")
+            {
+                double total = cost * quantity;
+                double gst = total * 0.15;
+                double totalgst = total + gst;
+                Console.WriteLine("Product: " + product);
+                Console.WriteLine("Quantity: " + quantity);
+                Console.WriteLine("Price: " + cost);
+                Console.WriteLine("Total: " + total);
+                Console.WriteLine("GST: " + gst);
+                Console.WriteLine("Total with GST: " + totalgst);
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice");
+            }
+
+
+
 
 
 
